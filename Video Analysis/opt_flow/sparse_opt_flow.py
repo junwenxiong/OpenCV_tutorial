133dc381263de13b98ce01d9bd772d0b2fe1a938
xjw <358975248@qq.com>
2020-07-12 16:31:57 +0800

    modified previous files

--- Video Analysis/opt_flow/sparse_opt_flow.py
+++ Video Analysis/opt_flow/sparse_opt_flow.py
@@ -1,58 +1,60 @@
-import cv2 as cv
+import cv2 
 import numpy as np
 
-# Parameters for Shi-Tomasi corner detection
+# 设置Shi-Tomasi角点检测参数
 feature_params = dict(maxCorners = 300, qualityLevel = 0.2, minDistance = 2, blockSize = 7)
-# Parameters for Lucas-Kanade optical flow
-lk_params = dict(winSize = (15,15), maxLevel = 2, criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
-# The video feed is read in as a VideoCapture object
-cap = cv.VideoCapture("resource\\Car.mp4")
-# Variable for color to draw optical flow track
-color = (0, 255, 0)
-# ret = a boolean return value from getting the frame, first_frame = the first frame in the entire video sequence
+
+# lucas kanada optical flow参数
+lk_params = dict(
+                winSize = (15,15),
+                maxLevel = 2,
+                criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+# 创造随机颜色
+color = (0,255,0)
+
+# 提取第一帧并找到其的转为灰度图像
+cap = cv2.VideoCapture("resource/Car.mp4")
 ret, first_frame = cap.read()
-# Converts frame to grayscale because we only need the luminance channel for detecting edges - less computationally expensive
-prev_gray = cv.cvtColor(first_frame, cv.COLOR_BGR2GRAY)
-# Finds the strongest corners in the first frame by Shi-Tomasi method - we will track the optical flow for these corners
-# https://docs.opencv.org/3.0-beta/modules/imgproc/doc/feature_detection.html#goodfeaturestotrack
-prev = cv.goodFeaturesToTrack(prev_gray, mask = None, **feature_params)
-# Creates an image filled with zero intensities with the same dimensions as the frame - for later drawing purposes
+prev_gray = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
+
+# 返回所有满足要求的角点的位置,角点为用Shi-Tomasi方法找到的最强的角点，后面使用optical flow追踪这些角点
+p0 = cv2.goodFeaturesToTrack(prev_gray, mask=None, **feature_params)
+
+# 因为角点位置为float，需要将其转为int型
+# corners = np.int0(corners)
+# 
+
 mask = np.zeros_like(first_frame)
 
 while(cap.isOpened()):
-    # ret = a boolean return value from getting the frame, frame = the current frame being projected in the video
     ret, frame = cap.read()
-    # Converts each frame to grayscale - we previously only converted the first frame to grayscale
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # Calculates sparse optical flow by Lucas-Kanade method
-    # https://docs.opencv.org/3.0-beta/modules/video/doc/motion_analysis_and_object_tracking.html#calcopticalflowpyrlk
-    next, status, error = cv.calcOpticalFlowPyrLK(prev_gray, gray, prev, None, **lk_params)
-    # Selects good feature points for previous position
-    # print("staus.shape:{}".format(status.shape))
-    good_old = prev[status == 1]
-    # Selects good feature points for next position
-    good_new = next[status == 1]
-    # Draws the optical flow tracks
-    for i, (new, old) in enumerate(zip(good_new, good_old)):
-        # Returns a contiguous flattened array as (x, y) coordinates for new point
-        a, b = new.ravel()
-        # Returns a contiguous flattened array as (x, y) coordinates for old point
-        c, d = old.ravel()
-        # Draws line between new and old position with green color and 2 thickness
-        mask = cv.line(mask, (a, b), (c, d), color, 2)
-        # Draws filled circle (thickness of -1) at new position with green color and radius of 3
-        frame = cv.circle(frame, (a, b), 3, color, -1)
-    # Overlays the optical flow tracks on the original frame
-    output = cv.add(frame, mask)
-    # Updates previous frame
-    prev_gray = gray.copy()
-    # Updates previous good feature points
-    prev = good_new.reshape(-1, 1, 2)
-    # Opens a new window and displays the output frame
-    cv.imshow("sparse optical flow", output)
-    # Frames are read by intervals of 10 milliseconds. The programs breaks out of the while loop when the user presses the 'q' key
-    if cv.waitKey(10) & 0xFF == ord('q'):
+
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # 使用Lucas-Kanade方法计算sparse optical flow
+    p1, status, error = cv2.calcOpticalFlowPyrLK(prev_gray, frame_gray, p0, None, **lk_params)
+
+    # 选择一些好的点
+    good_olds = p0[status == 1]
+    good_news = p1[status == 1]
+    # 画optical flow的轨迹
+    for i , (new, old) in enumerate(zip(good_news, good_olds)):
+        # 返回一个展平后的数组
+        x0, y0 = new.ravel()
+        x1, y1 = old.ravel()
+        # 从新坐标到旧坐标画一根color颜色的厚度为2的直线
+        mask = cv2.line(mask, (x0, y0), (x1, y1), color, 2)
+        # 在新坐标画一个半径为3的color颜色的厚度为-1的圆圈
+        frame = cv2.circle(frame, (x0, y0), 3, color, -1)
+    
+    img = cv2.add(frame, mask)
+    cv2.imshow("result", img)
+    prev_gray = frame_gray.copy()
+    p0= good_news.reshape(-1, 1, 2)
+
+    k = cv2.waitKey(10) & 0xff
+    if k == 27:
         break
-# The following frees up resources and closes all windows
+    
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
